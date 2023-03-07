c53226d1351225fb9dacee3324e179494921d7f7
Cecezobam <ifyzobam86@gmail.com>
2023-03-07 05:52:32 +0100

    adding words

---
+++ assignment.js
@@ -0,0 +1,30 @@
+
+      //The interest of a loan is 5 Naira per day on Hush Company
+      //James collected the sum of 25,000 Naira from Hush company for the period of 45 days
+    // The program below simulate the interest James is paying for   specific period, you are to arrange the program
+    //  using the appropriate structure that will run the program correctly 
+    
+
+
+    
+     // let company_interest = 5;
+    
+    let collected_loan = 25000;
+    
+    let days   = 23;
+    
+    let loan_period = 45;
+    
+    let company_interest = collected + ( company_interest* days);
+    
+    let message_1 = `Hello James, you will be paying ${company_interest} at the end of ${days} days, for collecting a loan of ${collected_loan}`; 
+    
+    let message_2 =`Hello James ,you exceeded your loan period , your loan period should not be more than ${loan_period} days`;
+
+    let messsage_3 = `Hello James you loan days should be more than one days`;
+
+    console.log(messages_1);
+    
+    console.log(message_2);
+    
+    console.log(messsage_3)
